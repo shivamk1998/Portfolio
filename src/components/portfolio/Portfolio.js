0fdb271896683ef403089ca00163c85f4a7e1226
shivamk1998 <shivamkumarsingh1998@gmail.com>
2023-07-12 02:57:37 +0530

    updated code.

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import "./portfolio.css"
+import React from "react";
+import "./portfolio.css";
+import recentworks from "./Data";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section className="work container section" id="work">
+      <h2 className="section_title">Recent Works</h2>
+      {/* <div className="work_filters">
+        <span className="work_item"></span>
 
-export default Portfolio
+      </div> */}
+
+      <div className="work_container grid">
+        {recentworks.map((work, id) => {
+          return (
+            <div className="work_card" key={id}>
+              <div className="work_thumbnail">
+                <img src={work.image} alt="" className="work_img" />
+                <div className="work_mask"></div>
+              </div>
+              <h3 className="work_title">{work.title}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
