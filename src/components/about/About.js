854e4478cad700276b953476dbaf52499771f3c0
shivamk1998 <shivamkumarsingh1998@gmail.com>
2023-07-11 12:17:53 +0530

    added more components.

---
+++ src/components/about/About.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import "./about.css";
+import Image from "../../assets/avatar-2.svg"
+
+const About = () => {
+  return (
+    <section className='about container section' id='about'>
+     <h2 className='section_title'>About Me</h2> 
+     <div className='about_container grid'>
+      <img src={Image} alt="" className='about_img'/>
+      <div className='about_data grid'>
+        <div className='about_info'>
+          <p className='about_description'>
+          Experienced Developer with a Strong Focus on Quality and Timely Delivery
+          Results-driven and adaptable developer with a proven ability to excel in both independent and collaborative work environments. Committed to delivering high-quality outcomes while meeting strict deadlines, I bring a track record of success and a dedication to excellence. Whether working autonomously or as part of a team, I possess a versatile skill set that enables me to consistently produce exceptional results. 
+          </p>
+          <a href='' className='btn'>Download CV</a>
+        </div>
+
+        <div className='about_skills grid'>
+          <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>HTML/CSS</h3>
+              <span className='skills_number'>90%</span>
+            </div>
+
+            <div className='skills_bar'>
+              <span className='skills_percentage html'></span>
+            </div>
+          </div>
+
+            <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>Github</h3>
+              <span className='skills_number'>75%</span>
+            </div>
+
+            <div className='skills_bar'>
+              <span className='skills_percentage github'></span>
+            </div>
+            </div>
+
+            <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>Javascript</h3>
+              <span className='skills_number'>70%</span>
+            </div>
+
+            <div className='skills_bar'>
+              <span className='skills_percentage javascript'></span>
+            </div>
+            </div>
+
+            <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>React</h3>
+              <span className='skills_number'>70%</span>
+            </div>
+
+            <div className='skills_bar'>
+              <span className='skills_percentage react'></span>
+            </div>
+            </div>
+
+            <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>Vue</h3>
+              <span className='skills_number'>65%</span>
+            </div>
+
+            <div className='skills_bar'>
+              <span className='skills_percentage vue'></span>
+            </div>
+            </div>
+
+            <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>Docker & Kubernates</h3>
+              <span className='skills_number'>65%</span>
+            </div>
+
+            <div className='skills_bar'>
+              <span className='skills_percentage dockub'></span>
+            </div>
+            </div>
+
+            <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>Cypress</h3>
+              <span className='skills_number'>65%</span>
+            </div>
+
+            <div className='skills_bar'>
+              <span className='skills_percentage cypress'></span>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/resume/Data.js
@@ -0,0 +1,36 @@
+const Data = [
+	{
+		id: 1,
+		category: "education",
+		icon: "icon-graduation",
+		year: "Aug-2017-Oct-2021",
+		title: "Btech-Electrical Engineering",
+		desc: "JECRC Jaipur."
+	},
+	{
+		id: 1,
+		category: "education",
+		icon: "icon-graduation",
+		year: "April-2015-Mar-2016",
+		title: "12th- PCM",
+		desc: "Indore (Madhya Pradesh)"
+	},
+	{
+		id: 1,
+		category: "education",
+		icon: "icon-graduation",
+		year: "Aug-2017-Oct-2021",
+		title: "10th",
+		desc: "Muzaffarpur (Bihar)"
+	},
+	{
+		id: 1,
+		category: "experience",
+		icon: "icon-briefcase",
+		year: "Jan-2022-Present",
+		title: "Apnaa Infotech",
+		desc: "Started as Associate Web Developer. My duties were to manage, Design, implement and support highly scalable applications and web services using Open-source technologies.Designing and developing applications on the front-end technologies."
+	}
+]
+
+export default Data;
