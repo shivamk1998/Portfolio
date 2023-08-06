d03a52fac0f5fa515d6c8434bf5f174fa8ade60d
shivamk1998 <shivamkumarsingh1998@gmail.com>
2023-08-07 01:50:02 +0530

    updated data.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,13 +14,13 @@ const About = () => {
           Experienced Developer with a Strong Focus on Quality and Timely Delivery
           Results-driven and adaptable developer with a proven ability to excel in both independent and collaborative work environments. Committed to delivering high-quality outcomes while meeting strict deadlines, I bring a track record of success and a dedication to excellence. Whether working autonomously or as part of a team, I possess a versatile skill set that enables me to consistently produce exceptional results. 
           </p>
-          <a href='' className='btn'>Download CV</a>
+          <a href='./Shivam_resume_new.pdf' download className='btn'>Download CV</a>
         </div>
 
         <div className='about_skills grid'>
           <div className='skills_data'>
             <div className='skills_titles'>
-              <h3 className='skills_name'>HTML/CSS</h3>
+              <h3 className='skills_name'>HTML|CSS|Bootstrap</h3>
               <span className='skills_number'>90%</span>
             </div>
 
@@ -76,7 +76,7 @@ const About = () => {
             <div className='skills_data'>
             <div className='skills_titles'>
               <h3 className='skills_name'>Docker & Kubernates</h3>
-              <span className='skills_number'>65%</span>
+              <span className='skills_number'>50%</span>
             </div>
 
             <div className='skills_bar'>
@@ -89,7 +89,18 @@ const About = () => {
               <h3 className='skills_name'>Cypress</h3>
               <span className='skills_number'>65%</span>
             </div>
+ 
+            <div className='skills_bar'>
+              <span className='skills_percentage cypress'></span>
+            </div>
+            </div>
 
+            <div className='skills_data'>
+            <div className='skills_titles'>
+              <h3 className='skills_name'>Familiar: TailwindCss | NodeJs | Electron JS</h3>
+              <span className='skills_number'>65%</span>
+            </div>
+ 
             <div className='skills_bar'>
               <span className='skills_percentage cypress'></span>
             </div>
--- src/components/services/Data.js
+++ src/components/services/Data.js
@@ -6,13 +6,13 @@ const services= [
   id:1,
   image:services_2,
   title:"Web Development",
-  description:"worked on full stack "
+  description:"A versatile Full Stack Developer with a passion for creating visually appealing and user-friendly websites. Proficient in modern web technologies and frameworks, adept at implementing responsive designs. "
 },
 {
   id:2,
   image:contentImg,
   title:"Content Writer",
-  description:"worked on full stack "
+  description:"A skilled Content Writer with a flair for crafting engaging and persuasive content across various niches. Created engaging blog posts on technical and non-technical topics, driving traffic and increasing user engagement"
 }
 ]
 
